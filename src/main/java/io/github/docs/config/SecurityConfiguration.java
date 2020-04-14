792d3cc346294ce5552ef39aee2c54c1b3b3f35a
Edwin Njeru <mailnjeru@gmail.com>
2020-04-14 14:49:09 +0300

    Remove registry API access to non-admin persons

--- src/main/java/io/github/docs/config/SecurityConfiguration.java
+++ src/main/java/io/github/docs/config/SecurityConfiguration.java
@@ -77,7 +77,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         .and()
             .authorizeRequests()
             .antMatchers("/api/authenticate").permitAll()
-            .antMatchers("/api/register").permitAll()
+            .antMatchers("/api/register").hasAuthority(AuthoritiesConstants.ADMIN)
             .antMatchers("/api/activate").permitAll()
             .antMatchers("/api/account/reset-password/init").permitAll()
             .antMatchers("/api/account/reset-password/finish").permitAll()
